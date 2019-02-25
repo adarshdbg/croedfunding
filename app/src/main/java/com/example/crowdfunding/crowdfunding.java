59639ff3912b131f3faa763ad2d7a4eb96f8b094
Adarsh <adarshdbg428@gmail.com>
2019-02-25 15:39:53 +0530

    i

---
+++ app/src/main/java/com/example/crowdfunding/crowdfunding.java
@@ -0,0 +1,13 @@
+package com.example.crowdfunding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class crowdfunding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crowdfunding);
+    }
+}
